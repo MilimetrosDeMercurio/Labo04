0eae118fff798e937d7b3761a8f41e233db4ce6f
María Lourdes <Mara Lourdes@VAIO>
2018-05-14 19:32:38 -0600

    Prueba inicial

---
+++ src/novos/Puntaje.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package novos;
+
+/**
+ *
+ * @author María Lourdes
+ */
+public class Puntaje {
+    
+    public int cant;
+    
+    //Aumenta el puntaje de cada jugador
+    public void Puntaje(){}
+    
+    //Muestra el puntaje del jugador
+    public void mostrarPuntaje(){}
+}
