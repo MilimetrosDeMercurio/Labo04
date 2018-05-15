2e74d9d90727efbfbbd1592223bd6a77aaefc2ab
María Lourdes <Mara Lourdes@VAIO>
2018-05-14 19:43:07 -0600

    Vidas

---
+++ src/novos/Vida.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package novos;
+
+/**
+ *
+ * @author María Lourdes
+ */
+public class Vida {
+    public int contacto;
+    public Obstaculo obstaculo;
+    
+    //Lleva la cuenta de vidas de un jugador
+    public void conteoVidas(){}
+    
+    //Muestra Vidas
+    public void mostrarVidas(){}
+    
+   
+}
