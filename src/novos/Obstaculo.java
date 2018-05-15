1a7337a9f9fb3e75c15836228d39817c893c302a
María Lourdes <Mara Lourdes@VAIO>
2018-05-14 19:39:47 -0600

    Obstaculo

---
+++ src/novos/Obstaculo.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package novos;
+
+/**
+ *
+ * @author María Lourdes
+ */
+public class Obstaculo {
+    public int dannio;
+    public Nave nave;
+    
+    //Relaciona las vidas con el puntaje para restar cuando entre en contacto con un obstaculo
+    public void RestarVida(){}
+    
+    //Muestra los obstaculos
+    public void mostrarObstaculo(){}
+}
