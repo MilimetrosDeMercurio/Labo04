3faf31d00a576af674e5fbe8e636dd5b87ffabdb
María Lourdes <Mara Lourdes@VAIO>
2018-05-14 19:41:56 -0600

    Planeta/PaDiFa

---
+++ src/novos/Planeta.java
@@ -0,0 +1,18 @@
+/*
+ Esta clase podría tener el patrón de diseño de FACTORY
+ */
+package novos;
+
+/**
+ *
+ * @author María Lourdes
+ */
+public class Planeta {
+    private int puntaje;
+    private int tamaño;
+   public Nave nave;
+   
+   public void mostrarPlaneta(){}
+   
+   
+}
